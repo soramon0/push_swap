0cb25242233fcff9afb5b405455b386011de6f6c
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-02-09 18:08:14 +0100

    feat: parse and validate input (WIP)

--- push_swap.c
+++ push_swap.c
@@ -10,10 +10,52 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
+#include "push_swap.h"
 
-int	main(void)
+int	ft_isnum(char *str)
 {
-	ft_putstr_fd("Hello World\n", 2);
+	ssize_t	i;
+
+	i = 0;
+	if (str[i] == '-')
+		i++;
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]) == 0)
+			return (-1);
+		i++;
+	}
+	return (1);
+}
+
+ssize_t	count_nums(char *input)
+{
+	char	**nums;
+	int		num;
+	ssize_t	i;
+
+	i = 0;
+	nums = ft_split(input, ' ');
+	if (nums == NULL)
+		return (-1);
+	while (nums[i] != NULL)
+	{
+		if (ft_isnum(nums[i]) == -1)
+			return (-1);
+		num = ft_atoi(nums[i]);
+		printf("%d\n", num);
+		i++;
+	}
+	return (i);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 2)
+		return (0);
+	if (count_nums(argv[1]) == -1)
+		ft_putstr_fd("Error\n", 2);
+	else
+		ft_putstr_fd("Valid\n", 1);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -13,6 +13,7 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include "stdio.h"
 # include "libft/libft.h"
 
 #endif
